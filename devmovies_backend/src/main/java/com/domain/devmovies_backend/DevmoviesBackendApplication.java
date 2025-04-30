e1b6211ed1e396c17f621947e97679268d53772b
dicutrim <diegocutrimcontato@gmail.com>
2025-04-30 16:17:18 -0300

    Inicialização do projeto e atualização do arquivo .gitignore

---
+++ devmovies_backend/src/main/java/com/domain/devmovies_backend/DevmoviesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.domain.devmovies_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmoviesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevmoviesBackendApplication.class, args);
+	}
+
+}
