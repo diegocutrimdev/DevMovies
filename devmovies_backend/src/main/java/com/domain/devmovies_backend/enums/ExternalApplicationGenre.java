4d91abbc13001d0a259d369e7180d2d073304c43
dicutrim <diegocutrimcontato@gmail.com>
2025-05-05 13:01:41 -0300

    Enum de gÊneros criado e implementação da classe DevMoviesServiceImpl

--- devmovies_backend/src/main/java/com/domain/devmovies_backend/enums/ExternalApplicationGenre.java
+++ devmovies_backend/src/main/java/com/domain/devmovies_backend/enums/ExternalApplicationGenre.java
@@ -1,4 +1,32 @@
 package com.domain.devmovies_backend.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum ExternalApplicationGenre {
+
+    ACAO(28, "Ação"),
+    AVENTURA(12, "Aventura"),
+    ANIMACAO(16, "Animação"),
+    COMEDIA(35, "Comédia"),
+    CRIME(80, "Crime"),
+    DOCUMENTARIO(99, "Documentário"),
+    DRAMA(18, "Drama"),
+    FAMILIA(10751, "Família"),
+    FANTASIA(14, "Fantasia"),
+    HISTORIA(36, "História"),
+    TERROR(27, "Terror"),
+    MUSICA(10402, "Música"),
+    MISTERIO(9648, "Mistério"),
+    ROMANCE(10749, "Romance"),
+    FICCAO_CIENTIFICA(878, "Ficção Científica"),
+    CINEMA_TV(10770, "Cinema TV"),
+    THRILLER(53, "Thriller"),
+    GUERRA(10752, "Guerra"),
+    FAROESTE(37, "Faroeste");
+
+    private final int id;
+    private final String descricao;
 }
--- devmovies_backend/src/main/java/com/domain/devmovies_backend/service/impl/DevMoviesServiceImpl.java
+++ devmovies_backend/src/main/java/com/domain/devmovies_backend/service/impl/DevMoviesServiceImpl.java
@@ -1,14 +1,50 @@
 package com.domain.devmovies_backend.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 import com.domain.devmovies_backend.dto.DevMoviesResponse;
+import org.springframework.core.ParameterizedTypeReference;
 import com.domain.devmovies_backend.service.DevMoviesService;
+import com.domain.devmovies_backend.dto.ExternalApplicationDto;
 import com.domain.devmovies_backend.enums.ExternalApplicationGenre;
 
+import java.util.*;
+
 @Service
+@RequiredArgsConstructor
 public class DevMoviesServiceImpl implements DevMoviesService {
+
+    private final RestTemplate restTemplate;
+
     @Override
     public DevMoviesResponse getMovie(Integer ano, String pais, String idioma, ExternalApplicationGenre genero) {
-        return null;
+
+        StringBuilder url = new StringBuilder("https://api.themoviedb.org/3/discover/movie");
+        String apiKey = "a34a59d5d9354d22c9cb7300e4cc9bc4";
+        url.append("?api_key=").append(apiKey);
+        url.append("&language=pt-BR");
+
+        if (ano != null) url.append("&primary_release_year=").append(ano);
+        if (pais != null) url.append("&with_origin_country=").append(pais);
+        if (idioma != null) url.append("&with_original_language=").append(idioma);
+        if (genero != null) url.append("&with_genres=").append(Objects.requireNonNull(genero).getId());
+
+        ResponseEntity<ExternalApplicationDto> response = restTemplate.exchange(
+                url.toString(),
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                });
+
+        List<ExternalApplicationDto.ExternalApplicationAttributesDto> movies = Optional.ofNullable(response.getBody())
+                .map(ExternalApplicationDto::getList).orElse(Collections.emptyList());
+
+        if (movies.isEmpty()) throw new RuntimeException("Nenhum filme encontrado com os filtros fornecidos.");
+        var randomMovie = movies.get(new Random().nextInt(movies.size()));
+
+        return new DevMoviesResponse(randomMovie.getMovie(), randomMovie.getPlot(), randomMovie.getReleaseDate());
     }
 }
